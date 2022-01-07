07e34c18fff958031c4d0099502017db618dec82
Igor Pedrosa <ilap1036@gmail.com>
2022-01-07 14:36:38 -0300

    barra de ferramentas e lazy loading routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rota | //conferindo se a rota está correta | //quando digitar corretamente irá abrir o modulo cdd - ao entrar na primeira página irá para cdd
+  {path: '', pathMatch: 'full', redirectTo: 'cdd'},
+  {
+    //organizando o que o path cdd irá retornar
+    path: 'cdd',
+    //onde importar a rota | Promise()
+    loadChildren: () => import('./cdd/cdd-routing.module').then(m => m.CddRoutingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cdd/cdd-routing.module.ts
+++ src/app/cdd/cdd-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClassesComponent } from './classes/classes.component';
+
+const routes: Routes = [
+  {path: '', component: ClassesComponent} //esse componente será exibido para a tal rota
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
